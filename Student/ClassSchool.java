5514442e9dda55e1d15b9a6e644d6df0a5ee0deb
MultyGigson <gigiomri@gmail.com>
2022-11-18 09:33:48 +0200

    Add intreface

---
+++ Student/ClassSchool.java
@@ -0,0 +1,31 @@
+class ClassSchool implements iclass {
+
+    private Student[] _students;
+    private int _count;
+    private String _name;
+
+    @Override
+    public float AvgGrades() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Boolean addSudent() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Student GetLeadStudent() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Student[] WeakStudent() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ Student/iclass.java
@@ -0,0 +1,12 @@
+public interface iclass {
+    @Override
+    String toString();
+
+    float AvgGrades();
+
+    Boolean addSudent();
+
+    Student GetLeadStudent();
+
+    Student[] WeakStudent();
+}
